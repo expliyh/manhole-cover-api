5e5ccfda9abfc44bbb6e2e537ebe777143a04070
Expliyh <expliyh@outlook.com>
2024-01-29 21:53:57 +0800

    Add post mapping /api/picture

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
@@ -11,3 +11,13 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("/api/picture")
+async def new_picture(
+        picture: UploadFile,
+        longitude: float,
+        latitude: float,
+        token: str
+):
+    return {"message": "TODO: Return Picture ID"}
