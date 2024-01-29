9b289a867c07cbb2c4b5e908a09e56e5e26266b8
Expliyh <expliyh@outlook.com>
2024-01-29 22:29:09 +0800

    Add mapper /api/position/get-formatted

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, UploadFile
 app = FastAPI()
 
 
+# BEGIN DEFAULT API
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -13,11 +15,23 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
+# BEGIN RESTFUL API
+
+
 @app.post("/api/picture")
 async def new_picture(
         picture: UploadFile,
         longitude: float,
-        latitude: float,
-        token: str
+        latitude: float
 ):
     return {"message": "TODO: Return Picture ID"}
+
+
+# BEGIN OTHER API
+
+@app.post("/api/position/get-formatted")
+async def get_position_string(
+        longitude: float,
+        latitude: float
+):
+    return {"message": "TODO: Return Position String"}
