a34cd3e4cf7984bbcb875822f60f56b9e9a1b909
Expliyh <expliyh@outlook.com>
2024-03-31 16:51:19 +0800

    登录

--- defines/AuditStatus.py
+++ defines/AuditStatus.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class AuditStatus(Enum):
     NOT_VIEWED = 'NOT_VIEWED'
+    PASS_VIEW = 'PASS_VIEW'
     VIEWED = 'VIEWED'
     SENT_TICKET = 'SENT_TICKET'
     TICKET_FINISHED = 'TICKET_FINISHED'
--- request_models/UserResponse.py
+++ request_models/UserResponse.py
@@ -7,3 +7,10 @@ class UserResponse:
     token: str | None
     role: None
     groups: [str]
+
+    def __init__(self, username: str, uid: int, token: str, role: None, groups: [str]):
+        self.username = username
+        self.uid = uid
+        self.token = token
+        self.role = role
+        self.groups = groups
