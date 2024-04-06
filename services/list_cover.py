a893d56f917dfe7f647a092ebc29646eed3a81a6
Expliyh <expliyh@outlook.com>
2024-04-06 20:32:01 +0800

    删除无用依赖

--- services/list_cover.py
+++ services/list_cover.py
@@ -1,13 +1,17 @@
-from models import cover_registry
+from models import cover_registry, user_registry
 from request_models.GetCoverListOptions import GetCoverListOptions
 
 
 async def list_cover(options: GetCoverListOptions):
     cover_list = await cover_registry.list_covers(options)
+    name_dict = {}
+    for i in cover_list:
+        name_dict[i.uploadUser] = (await user_registry.get_user_by_id(i.uploadUser)).fullname
     count = await cover_registry.count(options)
 
     return {
         'status': 'success',
         'cover_list': cover_list,
+        'username_dict': name_dict,
         'count': count
     }
