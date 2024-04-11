764f96f03d553747845a13e25283e06e6a76d964
Expliyh <expliyh@outlook.com>
2024-04-11 18:44:04 +0800

    修复登录请求参数位置，重置密码时验证用户，新建用户，添加用户，删除无用依赖，取消Docker镜像中的虚拟环境。

--- request_models/UserResponse.py
+++ request_models/UserResponse.py
@@ -8,6 +8,7 @@ class UserResponse:
     uid: int | None
     avatar: str | None
     email: str | None
+    full_name: str | None
     phone: str | None
     token: str | None
     role: None | str
@@ -16,7 +17,7 @@ class UserResponse:
 
     def __init__(self, username: str | None = None, uid: int | None = None, avatar: str | None = None,
                  email: str | None = None, phone: str | None = None, token: str | None = None, role: None | str = None,
-                 disabled: bool = False, groups: [str] = None):
+                 disabled: bool = False, groups: [str] = None, full_name: str | None = None):
         self.username = username
         self.uid = uid
         self.avatar = avatar
@@ -26,6 +27,7 @@ class UserResponse:
         self.role = role
         self.disabled = disabled
         self.groups = groups
+        self.full_name = full_name
 
 
 def from_orm(user: User, include_token=False) -> UserResponse:
@@ -38,5 +40,6 @@ def from_orm(user: User, include_token=False) -> UserResponse:
         disabled=user.disabled,
         avatar=user.avatar if user.avatar else "https://vip.123pan.cn/1814176066/DirectLink/%E8%8A%99%E5%8D%A1%E6%B4%9B%E6%96%AF_QuAn.jpg",
         role="Depreciated",
-        groups=user.groups
+        groups=user.groups,
+        full_name=user.fullname
     )
---
+++ services/edit_user.py
@@ -0,0 +1,23 @@
+from fastapi import HTTPException
+
+from models import user_registry
+from request_models import UserEditRequest
+
+
+async def edit_user(req: UserEditRequest, target_uid: int | None):
+    if target_uid is not None and target_uid != req.uid:
+        raise HTTPException(status_code=400, detail="参数冲突")
+    if req.uid is not None:
+        target_uid = req.uid
+    user = await user_registry.get_user_by_id(target_uid)
+    if user is None:
+        raise HTTPException(status_code=404, detail="用户不存在")
+    user.username = req.username or user.username
+    user.fullname = req.fullname or user.fullname
+    user.email = req.email or user.email
+    user.phone = req.phone or user.phone
+    user.avatar = req.avatar or user.avatar
+    await user_registry.merge(user)
+    return {
+        'message': 'success'
+    }
